cf534c224a71254a4943faf5593ef4a26b34a630
EmrozeJawed <eemilyllc@gmail.com>
2023-11-07 19:28:53 +0500

    Node-project

---
+++ Calculator/calculator.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+let answers : {
+    numberOne: number
+    numberTwo: number
+    operator: string
+} = await inquirer.prompt([
+
+    {
+     type: "number",
+     name: "numberOne",
+     message: "Please enter Your first number",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Please enter Your second number",
+       },
+       {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select operator",
+       },
+
+
+])
+
+let {numberOne, numberTwo, operator} = answers;
+if(numberOne && numberTwo && operator) {
+    let result: number = 0;
+    if(operator === "+") {
+        result = numberOne + numberTwo
+    }  if(operator === "-") {
+        result = numberOne - numberTwo 
+    }  if(operator === "*") {
+        result = numberOne * numberTwo
+    } if(operator === "/") {
+        result = numberOne / numberTwo
+    }
+   console.log("your result is:", result)
+} else {
+    console.log("Kindly enter valid input")
+}
